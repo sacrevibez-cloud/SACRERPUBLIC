1165ee79112294b22f4b0b0193df8d6fe294b1c2
sacrevibez-cloud <thomasdansou6@gmail.com>
2026-04-23 11:28:29 +0100

    Add Next.js configuration with i18n, reactStrictMode, swcMinify, and unoptimized images

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  i18n: {
+    locales: ['en', 'fr', 'de'],
+    defaultLocale: 'en',
+  },
+};
