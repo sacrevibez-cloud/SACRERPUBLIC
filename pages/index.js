f612aebeb69921f7a14c5b634a9a270a2ac5989d
sacrevibez-cloud <thomasdansou6@gmail.com>
2026-04-23 11:30:47 +0100

    Create pages/index.js file for the homepage layout

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Featured from '../components/Featured';
+import Footer from '../components/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Featured />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
